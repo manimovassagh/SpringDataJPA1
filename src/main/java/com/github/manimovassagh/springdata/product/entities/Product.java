78a8a3dc41c2b4a587b867c4876ef76d67b5d8af
manimovassagh <mani.mgh@gmail.com>
2022-03-02 19:48:08 +0100

    1

---
+++ src/main/java/com/github/manimovassagh/springdata/product/entities/Product.java
@@ -0,0 +1,5 @@
+package com.github.manimovassagh.springdata.product.entities;
+
+public class Product {
+
+}
---
+++ src/test/java/com/github/manimovassagh/springdata/product/ProductdataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.manimovassagh.springdata.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductdataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
