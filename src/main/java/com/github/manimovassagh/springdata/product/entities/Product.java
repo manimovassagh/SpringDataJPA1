4645fbb27e9fd48d8d9724b0b46adead5eb8b78d
manimovassagh <mani.mgh@gmail.com>
2022-03-02 19:56:36 +0100

    generate entity product

--- src/main/java/com/github/manimovassagh/springdata/product/entities/Product.java
+++ src/main/java/com/github/manimovassagh/springdata/product/entities/Product.java
@@ -1,5 +1,56 @@
 package com.github.manimovassagh.springdata.product.entities;
 
+import javax.persistence.Entity;
+
+@Entity
 public class Product {
 
+	private int id;
+	private String name;
+	private String desc;
+	private Double price;
+	public Product(int id, String name, String desc, Double price) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.desc = desc;
+		this.price = price;
+	}
+	
+	public Product() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDesc() {
+		return desc;
+	}
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", name=" + name + ", desc=" + desc + ", price=" + price + "]";
+	}
+	
+	
+	
 }
